1af4239cfa9edf14310f0aaa92abba418f2d0e41
qatestid12 <qatestid12@gmail.com>
2018-08-03 15:28:20 +0500

    Second commit

---
+++ Test Project Git/src/Mainclass.java
@@ -0,0 +1,17 @@
+
+public class Mainclass {
+
+	public static void main(String[] args) {
+
+		System.out.println("MY First CICD Project");
+
+		for(int x = 1; x < 31; x = x + 1) {
+	
+	  
+	         System.out.println(x + " HelloWorld");
+
+		
+	}
+
+	}
+}
